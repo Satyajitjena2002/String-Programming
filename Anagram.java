9f3951710ad3599dd520908fc5b6045f0df98aaa
Satyajitjena2002 <jenasatyajit891@gmail.com>
2025-09-03 23:52:19 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,33 @@
+package com_String;
+
+import java.security.PublicKey;
+
+public class Anagram {
+	public static void main(String[] args) {
+		String s1 = "race";
+		String s2 = "care";
+		if (isAnagram(s1, s2))
+			System.out.println(s1 + " and " + s2 + " both are Anagram string");
+		else {
+			System.out.println(s1 + " and " + s2 + " both are not Anagram string");
+		}
+	}
+
+	public static boolean isAnagram(String s1, String s2) {
+		while (true) {
+			if (s1.length() != s2.length()) {
+				return false;
+			}
+			if (s1.length() == 0 && s2.length() == 0) {
+				return true;
+			} else {
+				char ch = s1.charAt(0);
+
+				s1 = s1.replace(ch + "", "");
+				s2 = s2.replace(ch
++ "", "");
+
+			}
+		}
+	}
+}
